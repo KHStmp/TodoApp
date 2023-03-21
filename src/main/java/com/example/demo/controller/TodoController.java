a9186a8b2f73da5f90df98bdd9f5e9eb8a0f1666
KHStmp <burdock2nd@duksung.ac.kr>
2023-03-21 21:49:46 +0900

    3/20일 수업 완료

--- src/main/java/com/example/demo/controller/TodoController.java
+++ src/main/java/com/example/demo/controller/TodoController.java
@@ -1,21 +1,22 @@
 package com.example.demo.controller;
 
+import com.example.demo.dto.TodoDTO;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("todo")
 public class TodoController {
+
     @GetMapping("/testTodo")
-    public String testController() {
-        return "Hello World, 김현서";
+    public ResponseEntity<?> todo(){
+        TodoDTO response = TodoDTO.<String,Boolean>builder().id("123").title("김현서").done(true).build();
+        return ResponseEntity.ok().body(response);
     }
 
-    @GetMapping("/{id}")
-    public String testControllerWithPathVariables(@PathVariable(required = false) int id)
-    {
-        return "Hello World! ID " + id;
-    }
 }
