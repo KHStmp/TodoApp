db00cd8c0a7a043c812078b4966926ce592f1781
KHStmp <burdock2nd@duksung.ac.kr>
2023-03-22 13:11:05 +0900

    3/22일 수업 완료(2.2절 완료)

--- src/main/java/com/example/demo/controller/TodoController.java
+++ src/main/java/com/example/demo/controller/TodoController.java
@@ -1,6 +1,9 @@
 package com.example.demo.controller;
 
+import com.example.demo.dto.ResponseDTO;
 import com.example.demo.dto.TodoDTO;
+import com.example.demo.service.TodoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +15,18 @@ import java.util.List;
 @RestController
 @RequestMapping("todo")
 public class TodoController {
+
+    @Autowired
+    private TodoService service;
+
+    @GetMapping("/test")
+    public ResponseEntity<?> testTodo() {
+        String str = service.testService();
+        List<String> list = new ArrayList<>();
+        list.add(str);
+        ResponseDTO<String> response = ResponseDTO.<String>builder().data(list).build();
+        return ResponseEntity.ok().body(response);
+    }
 
     @GetMapping("/testTodo")
     public ResponseEntity<?> todo(){
