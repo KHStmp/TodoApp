32820bfc5614bbf48763bac2aa77d2d6e4102d81
KHStmp <burdock2nd@duksung.ac.kr>
2023-03-27 16:17:35 +0900

    3/27일 수업 완료(2.3절 retrive 까지 완료)

--- src/main/java/com/example/demo/persistence/TodoRepository.java
+++ src/main/java/com/example/demo/persistence/TodoRepository.java
@@ -1,6 +1,7 @@
 package com.example.demo.persistence;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import com.example.demo.model.TodoEntity;
 
@@ -8,6 +9,7 @@ import java.util.List;
 
 @Repository
 public interface TodoRepository extends JpaRepository<TodoEntity, String> {
+    //@Query(value = "select * from Todo t where t.userId = ?1", nativeQuery = true)
     List<TodoEntity> findByUserId(String userId);
 
 }
--- src/main/java/com/example/demo/service/TodoService.java
+++ src/main/java/com/example/demo/service/TodoService.java
@@ -1,10 +1,14 @@
 package com.example.demo.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.example.demo.model.TodoEntity;
 import com.example.demo.persistence.TodoRepository;
 
+import java.util.List;
+
+@Slf4j
 @Service
 public class TodoService {
 
@@ -20,4 +24,33 @@ public class TodoService {
         TodoEntity savedEntity = repository.findById(entity.getId()).get();
         return savedEntity.getTitle();
     }
+
+    public List<TodoEntity> create(final TodoEntity entity) {
+        // Validations
+        validate(entity);
+
+        repository.save(entity);
+
+        log.info("Entity Id : {} is saved.", entity.getId());
+
+        return repository.findByUserId(entity.getUserId());
+    }
+
+    // 리팩토링한 메서드
+    private static void validate(TodoEntity entity) {
+        if(entity == null) {
+            log.warn("Entity cannot be null.");
+            throw new RuntimeException("Entity cannot be null.");
+        }
+
+        if(entity.getUserId() == null) {
+            log.warn("Unknown user.");
+            throw new RuntimeException("Unknown user.");
+        }
+    }
+
+    public List<TodoEntity> retrieve(final String userId) {
+        return repository.findByUserId(userId);
+    }
+
 }
